b1ce1915b28aae098b9150040edcfdc481d0a115
apreble21 <61958972+apreble21@users.noreply.github.com>
2020-08-20 14:49:53 -0600

    part 1

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,126 @@
+// URL to earthquake data
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// GET request, and function to handle returned JSON data
+d3.json(queryUrl, function(data) {
+  
+  var earthquakes = L.geoJSON(data.features, {
+    onEachFeature : addPopup,
+    pointToLayer: addMarker
+  });
+// call function to create map
+createMap(earthquakes);
+
+});
+// function to determine marker size based on magnitude
+function markerSize(magnitude) {
+    return magnitude * 5;
+}
+
+// function to return the color based on magnitude
+function markerColor(magnitude) {
+  if (magnitude > 4) {
+    return 'red'
+  } else if (magnitude > 3) {
+    return 'orange'
+  } else if (magnitude > 2) {
+    return 'yellow'
+  } else {
+    return 'green'
+  }
+}
+
+// function for opacity based on magnitude
+function markerOpacity(magnitude) {
+  if (magnitude > 5) {
+    return 1
+  } else if (magnitude > 4) {
+    return .80
+  } else if (magnitude > 3) {
+    return .70
+  } else if (magnitude > 2) {
+    return .60
+  } else if (magnitude > 1) {
+    return .50
+  } else {
+    return .40
+  }
+}
+// function to add markers 
+function addMarker(feature, location) {
+  var options = {
+    stroke: false,
+    fillOpacity: markerOpacity(feature.properties.mag),
+    color: markerColor(feature.properties.mag),
+    fillColor: markerColor(feature.properties.mag),
+    radius: markerSize(feature.properties.mag)
+  }
+
+  return L.circleMarker(location, options);
+
+}
+
+// function to add pop-up with information
+function addPopup(feature, layer) {
+    // Give each feature a popup describing the place and time of the earthquake
+    return layer.bindPopup(`<h3> ${feature.properties.place} </h3> <hr> <h4>Magnitude: ${feature.properties.mag} </h4> <p> ${Date(feature.properties.time)} </p>`);
+}
+
+//layers
+function createMap(earthquakes) {
+
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "light-v10",
+      accessToken: "pk.eyJ1IjoiYXByZWJsZTIxIiwiYSI6ImNrY2h1YmdvdjBhd2gyd21pMGszYmM3MnIifQ.nhcXk1qrAKgrRFZbMF4i6Q"
+    });
+  
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "dark-v10",
+      accessToken: "pk.eyJ1IjoiYXByZWJsZTIxIiwiYSI6ImNrY2h1YmdvdjBhd2gyd21pMGszYmM3MnIifQ.nhcXk1qrAKgrRFZbMF4i6Q"
+    });
+  
+    // Base map layer
+    var baseMaps = {
+      "Street Map": lightmap,
+      "Dark Map": darkmap
+    };
+  
+    // Overlay map of earthquakes 
+    var overlayMaps = {
+      Earthquakes: earthquakes
+    };
+  
+    // Create inital map settings
+    var myMap = L.map("map", {
+      center: [37.09, -95.71],
+      zoom: 5,
+      layers: [lightmap, earthquakes]
+    });
+  
+    // creating the legend
+    var legend = L.control({position: 'bottomright'});
+
+    // add legend to map
+    legend.onAdd = function () {
+    
+        var div = L.DomUtil.create('div', 'info legend')
+        
+        div.innerHTML = "<h3>Magnitude Legend</h3><table><tr><th>>= 4</th><td>Red</td></tr><tr><th>>= 3</th><td>Orange</td></tr><tr><th>>= 2</th><td>Yellow</td></tr><tr><th>< 2</th><td>Green</td></tr></table>";
+
+        return div;
+    };
+    
+    legend.addTo(myMap);
+
+    // Create a layer control
+    // Pass in our baseMaps and overlayMaps
+    // Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);
+
+  }
